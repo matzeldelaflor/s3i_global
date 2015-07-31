385cc90fa8aa167fa0f87624680b2e9bedd1c3c0
Mark Matzeldelaflor <matzeldelaflor@gmail.com>
2015-07-30 21:07:55 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	$(".navbar-toggle").mouseover(function() {
+		$(".icon-bar").css("background-color", "red");
+	});
+	$(".navbar-toggle").mouseout(function() {
+		$(".icon-bar").css("background-color", "#777");
+	});
+
+	/* ///////////////////////////////////
+	/// code for fomr on contact page ///
+	*///////////////////////////////////
+
+	
+	/* ///////////////////////////////////////
+	/// END code for fomr on contact page ///
+	*///////////////////////////////////////
+
+});
